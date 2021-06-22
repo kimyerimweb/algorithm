9958bc0f86ced355e948cbf6d0530adebba6a8f5
kimyerimweb <kimyerimweb@gmail.com>
2021-06-22 21:08:20 +0900

    DFS/BFS

---
+++ 그래프/1260.js
@@ -0,0 +1,71 @@
+const DFS = (num) => {
+    visitedDfs[num] = true
+    dfsResult.push(num)
+
+    for(let i = 1; i < graph.length;i++){
+        if(graph[num][i] === 1 && visitedDfs[i] === false){
+            DFS(i)
+        }
+    }
+    return
+}
+
+const BFS = (num) => {
+    let queue = []
+    queue.push(num)
+    bfsResult.push(num)
+
+    while(queue.length !== 0){
+        let shiftQueue = queue.shift()
+        visitedBfs[shiftQueue] = true
+
+        for(let i = 1; i < graph.length;i++){
+            if(graph[shiftQueue][i] === 1 && visitedBfs[i] === false){
+                visitedBfs[i] = true
+                queue.push(i)
+                bfsResult.push(i)
+            }
+        }
+    }
+    return
+}
+
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+let input = []
+let graph = []
+let visitedDfs = []
+let visitedBfs = []
+let dfsResult = []
+let bfsResult = []
+
+rl.on("line",function(line){
+    input.push(line.toString())
+}).on("close",function(){
+    let [node,edge,num] = input
+        .shift()
+        .split(' ')
+        .map(el => parseInt(el))
+
+    graph = Array.from(Array(node+1),()=> Array(node + 1).fill(0))
+
+    for(let i of input){
+        let [x,y] = i.split(' ').map(el => parseInt(el))
+        graph[x][y] = 1
+        graph[y][x] = 1
+    }
+
+    visitedDfs = new Array(node + 1).fill(false)
+    visitedBfs = new Array(node + 1).fill(false)
+
+    DFS(num)
+    BFS(num)
+
+    console.log(dfsResult.join(' ') + '\n' + bfsResult.join(' '))
+    process.exit()
+})
