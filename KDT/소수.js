faececa6b262ac052425f438543c4011f79f388f
kimyerimweb <kimyerimweb@gmail.com>
2022-04-07 23:33:00 +0900

    소수 효율성 테스트 통과하도록 수정

--- KDT/소수.js
+++ KDT/소수.js
@@ -3,7 +3,7 @@ function isPrimeNumber(x) {
   if (x === 2) return true;
   if (x === 3) return true;
 
-  for (let i = 2; i <= parseInt(Math.sqrt(x)); i++) {
+  for (let i = 2; i <= Math.sqrt(x); i++) {
     if (x % i === 0) {
       return false;
     }
