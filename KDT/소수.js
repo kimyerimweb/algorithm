49c4fbd55dcbf098d0b9b95085f0bb6f7af0f35f
kimyerimweb <kimyerimweb@gmail.com>
2022-04-07 23:31:49 +0900

    소수 틀린 부분 수정

--- KDT/소수.js
+++ KDT/소수.js
@@ -1,15 +1,16 @@
 function isPrimeNumber(x) {
-    if(x === 1) return false
-    if(x === 2) return true
+  if (x === 1) return false;
+  if (x === 2) return true;
+  if (x === 3) return true;
 
-    for(let i = 2; i <= parseInt(Math.sqrt(x)); i++){
-        if(x % i === 0){
-            return false
-        }
-        return true
+  for (let i = 2; i <= parseInt(Math.sqrt(x)); i++) {
+    if (x % i === 0) {
+      return false;
     }
+  }
+  return true;
 }
 
-console.log(isPrimeNumber(1))
-console.log(isPrimeNumber(2))
-console.log(isPrimeNumber(4))
+console.log(isPrimeNumber(1));
+console.log(isPrimeNumber(2));
+console.log(isPrimeNumber(4));
